d8f1c392ee8a280ad031d6f32df51183ea6f6fe8
Godisouleymane <ssabiougodi@gmail.com>
2025-05-14 04:25:01 +0100

    premier commit

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# Vérifier s'il y a au moins un argument
-if [ $# -eq 0 ]; then
-    echo "Aucun argument fourni."
-    echo "Utilisation : ./print_args.sh arg1 arg2 ..."
-    exit 1
-fi
-
-# Affichage des arguments un par un
 count=1
-for arg in "$@"
-do
-    echo "Argument $count: $arg"
-    count=$((count + 1))
+for arg in "$@"; do
+    echo "Argument $count : $arg"
+    count=$((count++))
 done
--- find_string.sh
+++ find_string.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-chaine=$1
-fichier=$2
+read cafe 
+read chaine
 
-if [ ! -f "$fichier" ]; then
-    echo "Erreur : le fichier '$fichier' n'existe pas."
-    exit 1
-fi
-# Rechercher la chaîne avec grep
-grep --color=always -n "$chaine" "$fichier"
-
-# Vérifier si grep a trouvé quelque chose
-if [ $? -ne 0 ]; then
-    echo "La chaîne '$chaine' n'a pas été trouvée dans le fichier '$fichier'."
+if grep -q "$chaine" "$cafe"; then
+    echo "La chaîne '$chaine' a été trouvée dans $cafe."
+else
+    echo "La chaîne '$chaine' n'a pas été trouvée dans $cafe."
 fi
