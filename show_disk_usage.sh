44c33182c521ae6d85b4974b4ff2b25f40a68481
Godisouleymane <ssabiougodi@gmail.com>
2025-05-14 04:31:39 +0100

    premier commit

--- show_disk_usage.sh
+++ show_disk_usage.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-echo "--- Utilisation de l'espace disque (tous les systèmes de fichiers) ---"
 df -h
-echo "-------------------------------------------------------------------"
-
-
-if [ "$utilisation" -ge "$seuil_utilisation" ]; then
-  echo -e "\n AVERTISSEMENT : L'utilisation de la partition '$partition_a_verifier' a atteint ou dépassé $utilisation% !"
-fi
-
-# Optionnel : Afficher également la taille du répertoire courant de manière lisible
-echo -e "\n--- Taille du répertoire courant ---"
-du -sh .
-echo "-------------------------------------"
