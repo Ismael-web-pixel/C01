d70de7df619c9b2953e7d61550e79affb5edceaf
Godisouleymane <ssabiougodi@gmail.com>
2025-05-14 03:28:12 +0100

    premier commit

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo "Utilisation : ./simple_calculator.sh nombre1 nombre2 opérateur"
-    echo "Exemple : ./simple_calculator.sh 4 2 +"
-    exit 1
-fi
-
 a=$1
 b=$2
 op=$3
 
-# Tester l'opérateur
-if [[ "$op" = "+" ]]; then
-    result=$((a + b))
-elif [[ "$op" = "-" ]]; then
-    result=$((a - b))
-elif [[ "$op" = "*" ]]; then
-    result=$((a * b))
-elif [[ "$op" = "/" ]]; then
-    if [ "$b" -eq 0 ]; then
-        echo "Erreur : Division par zéro impossible"
-        exit 1
-    fi
-    result=$((a / b))
-else
-    echo "Opérateur inconnu"
-    exit 1
-fi
+read a b op
 
-# Afficher le résultat
+if [ "$op" = "+" ]; then
+    result=$(($a + $b))
+
+elif [ "$op" = "*" ]; then
+    result=$(($a + $b))
+
+elif [ "$op" = "-" ]; then
+    result=$(($a + $b))
+
+elif [ "$op" = "/" ]; then
+    result=$(($a / $b))
+fi
 echo "Résultat : $result"
